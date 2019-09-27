cd365ca2522afaeb9a6ed1499d0aa259d539a3ea
Krithika Subramanian <ksubram2@students.kennesaw.edu>
2019-09-27 15:22:08 -0400

    Solved errors

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import images from "./images.json";
 import "./App.css";
 
 class App extends Component {
-
   state = {
     images,
     userScore: 0,
@@ -15,7 +14,9 @@ class App extends Component {
   };
 
   shuffleArray(arr) {
-    return arr.sort(() => { return Math.random() - 0.5 });
+    return arr.sort(() => {
+      return Math.random() - 0.5;
+    });
   }
 
   isClicked = id => {
@@ -29,53 +30,54 @@ class App extends Component {
 
     // console.log("newArr:", newArr)
     this.setState({ images: newArr });
-  }
+  };
 
   clickHandler = event => {
     event.preventDefault();
     const { id } = event.target;
-    const idInt = parseInt(id)
+    const idInt = parseInt(id);
     const image = this.state.images.filter(img => img.id === idInt).pop();
     console.log("image", image);
     console.log(image.clicked === true);
 
     if (image.clicked === true) {
-      this.setState({ userScore: 0 })
-      console.log('You lose!');
+      this.setState({ userScore: 0 });
+      console.log("You lose!");
       this.setState({
         images: images
-      })
+      });
     } else {
       this.isClicked(id);
-      console.log('else', id)
+      console.log("else", id);
       const newScore = this.state.userScore + 1;
       this.setState({
         userScore: newScore
-      })
+      });
       if (newScore > this.state.highScore) {
         this.setState({
           highScore: newScore
-        })
+        });
       }
-
     }
     const newArr = this.shuffleArray(images);
     this.setState({ images: newArr });
     // console.log('Image Array:', this.state.images)
-  }
+  };
 
   render() {
     return (
       <Wrapper>
-        <Header score={this.state.userScore}
-          highScore={this.state.highScore}>Clicky Game</Header>
+        <Header score={this.state.userScore} highScore={this.state.highScore}>
+          Clicky Game
+        </Header>
         {this.state.images.map(item => (
           <Card
             clickHandler={this.clickHandler}
             clicked={item.clicked}
             id={item.id}
             key={item.id}
-            image={item.imgFile} />
+            image={item.imgFile}
+          />
         ))}
       </Wrapper>
     );
